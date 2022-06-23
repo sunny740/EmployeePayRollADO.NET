294329232c597cc16e3261678c0e0e9557d18c2e
Sunny <sunnysejwarvitm005@gmail.com>
2022-06-23 07:10:10 -1200

    [Sunny] Add : Update The Employee Salary

--- EmployeePayRollADO.NET/EmployeePayRollADO.NET/Employee.cs
+++ EmployeePayRollADO.NET/EmployeePayRollADO.NET/Employee.cs
@@ -85,5 +85,23 @@ namespace EmployeePayRollADO.NET
             }
             sqlConnection.Close();
         }
+        public void UpdateSalary()
+        {
+            sqlConnection.Open();
+            string query = "update employee_payroll set BasicPay=3000000 where EmployeeName= 'Terrrisa'";
+
+            SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+            int result = sqlCommand.ExecuteNonQuery();
+            if (result != 0)
+            {
+                Console.WriteLine("Updated");
+            }
+            else
+            {
+                Console.WriteLine("Not Updated");
+            }
+            sqlConnection.Close();
+            GetSqlData();
+        }
     }
 }
